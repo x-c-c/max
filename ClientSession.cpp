6c0a6ccea143608c0f70751ab8c95b411f3c5833
x-c-c <mipodotovic@gmail.com>
2026-07-20 22:43:58 +0300

    finish ClientSession tomorow

--- ClientSession.cpp
+++ ClientSession.cpp
@@ -1,5 +1,5 @@
 #include "ClientSession.h"
-#include "Packets.h"
+
 
 
 ClientSession::ClientSession(int socketFd): socketFd_(socketFd){}
@@ -18,7 +18,7 @@ void ClientSession::handleRead()
 		// Добавляем прочитанные байты в буфер
 		size_t oldSize = readBuffer_.size();
 		readBuffer_.resize(oldSize + bytesRead);
-		std::memcpy(readBuffer_.data() + oldSize, tmp, sizeof(bytesRead));
+		std::memcpy(readBuffer_.data() + oldSize, tmp, bytesRead);
 		
 		// Пытаемся выделить целые пакеты
 		tryExtractPackets();
@@ -39,38 +39,101 @@ void ClientSession::handleRead()
 		}
 	}
 }
+bool ClientSession::extractPacket(PacketHeaderRaw& header, std::vector<uint8_t>& body)
+{
+	if (readBuffer_.size() < sizeof(PacketHeaderRaw))	// Минимальный размер заголовка = 12 байт (на 20.07.2026)
+		return false;
+	
+	// Копируем заголовок и сразу переводим все поля в хостовый порядок
+	std::memcpy(&header, readBuffer_.data(), sizeof(header));
+	header.type_       = ntohs(header.type_);
+	header.messageID_  = ntohl(header.messageID_);
+	header.sessionID_  = ntohl(header.sessionID_);
+	header.messageLen_ = ntohs(header.messageLen_);
 
+	size_t totalSize = sizeof(PacketHeaderRaw) + header.messageLen_;
+	if (readBuffer_.size() < totalSize)					// Если прочитано байт меньше чем требуется для выделения целого пакета, то ждём ещё
+		return false;
+
+	// Извлекаем тело
+	body.assign(readBuffer_.begin() + sizeof(PacketHeaderRaw), readBuffer_.begin() + totalSize);
+
+	// Удаляем обработанные байты из буфера
+	readBuffer_.erase(readBuffer_.begin(), readBuffer_.begin() + totalSize);
+	return true;
+}
 void ClientSession::tryExtractPackets()
 {
-	while(true)
+	PacketHeaderRaw header;
+	std::vector<uint8_t> body;
+	while (extractPacket(header, body))
 	{
-		if (readBuffer_.size() < sizeof(PacketHeaderRaw))	// Минимальный размер заголовка: 12 байт (на 20.07.2026)
-			break;
-		// Читаем заголовок
-		PacketHeaderRaw header;
-		std::memcpy(&header, readBuffer_.data(), sizeof(header));
-		uint16_t messageLen = ntohs(header.messageLen_);
-		size_t totalSize = sizeof(PacketHeaderRaw) + messageLen;
-		
-		// Если прочитано байт меньше чем требуется для выделения целого пакета, то ждём ещё
-		if (readBuffer_.size() < totalSize)
-			break;
-		
-		// Извлекаем полный пакет отдельный вектор
-		std::vector<uint8_t> packet(readBuffer_.begin(), readBuffer_.begin() + totalSize);
-		
-		// Удаляем байты, записанные в отдельный вектор, из буфера
-		readBuffer_.erase(readBuffer_.begin() + totalSize);
-				
-		// потом тут будет роутер
-		uint16_t type = ntohs(header.type_);
-		std::cout << "[Client " << socketFd_ << "] packet extracted, type=0x"
-				  << std::hex << type << std::dec
-				  << ", msgID=" << ntohl(header.messageID_)
-				  << ", bodyLen=" << messageLen << std::endl;
-	
+		processPacket(header, body);
+		if (closed_)
+			return;
 	}
 }
+/*
+void ClientSession::processPacket(const PacketHeaderRaw& header, const std::vector<uint8_t>& body)
+{
+	switch (static_cast<PacketType>(header.type_))
+	{
+		case PacketType::ConnectRequest:
+		{
+			ConnectRequestPacket packet;
+			Serializer::parseConnectRequestPacket(body, packet);
+			handleConnectRequest(header.messageID_, header.sessionID_);
+		}
+		break;
+
+		case PacketType::RegisterRequest:
+		{
+			RegisterRequestPacket packet;
+			if (Serializer::parseRegisterRequestPacket(body, packet))
+				handleRegisterRequest(header.messageID_, header.sessionID_, packet);
+		}
+		break;
+
+		case PacketType::AuthRequest:
+		{
+			AuthRequestPacket packet;
+			if (Serializer::parseAuthRequestPacket(body, packet))
+				handleAuthRequest(header.messageID_, header.sessionID_, packet);
+		}
+		break;
+
+		case PacketType::MessageSend:
+		{
+			MessageSendPacket packet;
+			if (Serializer::parseMessageSendPacket(body, packet))
+				handleMessageSend(header.messageID_, header.sessionID_, packet);
+		}
+		break;
+
+		case PacketType::DisconnectRequest:
+			handleDisconnectRequest();
+		break;
+
+		default:
+			std::cerr << "[Client " << socketFd_ << "] unknown packet type 0x" << std::hex << header.type_ << std::dec << std::endl;
+		break;
+	}
+}
+*/
+/*
+void ClientSession::sendResponse(const std::vector<uint8_t>& data);
+// Обработчики конкретных пакетов
+void ClientSession::handleConnectRequestPacket(uint32_t messageID, uint32_t sessionID);
+void ClientSession::handleRegisterRequestPacket(uint32_t messageID, uint32_t sessionID, const RegisterRequestPacket& packet);
+void ClientSession::handleAuthRequestPacket(uint32_t messageID, uint32_t sessionID, const AuthRequestPacket& packet);
+void ClientSession::handleMessageSendPacket(uint32_t messageID, uint32_t sessionID, const MessageSendPacket& packet);
+void ClientSession::handleDisconnectRequestPacket();
+*/
+
+
+
+
+
 
 
 
@@ -84,3 +147,6 @@ void ClientSession::closeSession()
 	close(socketFd_);
 	std::cout << "[Client " << socketFd_ << "] session closed" << std::endl;
 }
+
+
+
--- ClientSession.h
+++ ClientSession.h
@@ -6,7 +6,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-
+#include "Packets.h"
 class ClientSession
 {
 private:
@@ -16,6 +16,18 @@ private:
 	static constexpr size_t MAX_BUFFER_SIZE = 64 * 1024;	// 64 kB
 	
 	void tryExtractPackets();
+	bool extractPacket(PacketHeaderRaw& header, std::vector<uint8_t>& body);
+	
+	void processPacket(const PacketHeaderRaw& header, const std::vector<uint8_t>& body);
+	void sendResponse(const std::vector<uint8_t>& data);
+
+	// Обработчики конкретных пакетов
+	void handleConnectRequestPacket(uint32_t messageID, uint32_t sessionID);
+	void handleRegisterRequestPacket(uint32_t messageID, uint32_t sessionID, const RegisterRequestPacket& packet);
+	void handleAuthRequestPacket(uint32_t messageID, uint32_t sessionID, const AuthRequestPacket& packet);
+	void handleMessageSendPacket(uint32_t messageID, uint32_t sessionID, const MessageSendPacket& packet);
+	void handleDisconnectRequestPacket();
+	
 public:
 	explicit ClientSession(int socketFd);
 	~ClientSession();
